b8808e0dfae8c2c57c1ac1c01e5c586a112f38fb
Aria Adi Pramesta <ariaapramesta@gmail.com>
2025-05-31 15:12:32 +0700

    Update: install nodemon for development and create connection to postgresql using pg pool

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  user: 'pramesta',
+  password: 'pramestauser',
+  host: 'localhost',
+  database: 'posdb',
+  port: 5432,
+});
+
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users')(pool);
 
 var app = express();
 
@@ -23,12 +33,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
