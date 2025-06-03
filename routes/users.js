902bcf37f10bb237c370aa114b191ab46bef3f3d
Aria Adi Pramesta <ariaapramesta@gmail.com>
2025-06-03 10:10:36 +0700

    Update: create edit user fiture

--- routes/users.js
+++ routes/users.js
@@ -15,6 +15,17 @@ module.exports = function (db) {
     }
   });
 
+  router.get('/', isLoggedIn, async function (req, res, next) {
+    try {
+      const result = await db.query('SELECT * FROM users')
+
+      console.log(result.rows)
+
+      res.render('users/users', { user: req.session.user, data: result.rows });
+    } catch (error) {
+      console.log(error)
+    }
+  });
 
   router.get('/add', isLoggedIn, function (req, res, next) {
     res.render('users/users-add', { user: req.session.user });
@@ -30,6 +41,57 @@ module.exports = function (db) {
       res.send('Failed to insert data')
     }
   });
+
+  router.get('/edit/:id', isLoggedIn, async function (req, res, next) {
+    try {
+      const userId = req.params.id;
+
+      const result = await db.query('SELECT * FROM users WHERE "userId" = $1', [userId]);
+
+      if (result.rows.length === 0) {
+        return res.status(404).send('Data not found');
+      }
+
+      const item = result.rows[0];
+      res.render('users/users-edit', { item });
+    } catch (error) {
+      console.error(error);
+      res.send('Error while trying to get data');
+    }
+  });
+
+  router.post('/edit/:id', isLoggedIn, async function (req, res, next) {
+    try {
+      const userId = req.params.id;
+      const { email, name, role } = req.body;
+
+      await db.query(
+        'UPDATE users SET email = $1, name = $2, role = $3 WHERE "userId" = $4',
+        [email, name, role, userId]
+      );
+
+      res.redirect('/users');
+    } catch (error) {
+      console.error(error);
+      res.send('Update data error');
+    }
+  });
+
+  router.post('/delete/:id', async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+      if (!userId) return res.status(400).send('Invalid user ID');
+
+      await db.query('DELETE FROM users WHERE "userId" = $1', [userId]);
+
+      res.redirect('/users');
+    } catch (err) {
+      console.error('Delete user error:', err);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+
 
   return router;
 }
