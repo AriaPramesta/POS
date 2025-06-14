723652b5a9d763296436d5f8f52d0118554df5c6
Aria Adi Pramesta <ariaapramesta@gmail.com>
2025-06-14 13:50:13 +0700

    Update: purchase item

--- routes/purchases.js
+++ routes/purchases.js
@@ -1,17 +1,29 @@
 var express = require('express');
 const { isLoggedIn } = require('../helper/util');
-const { Purchase, Supplier } = require('../models')
+const { Purchase, Supplier, User, Good, PurchaseItem } = require('../models')
+const moment = require('moment');
 var router = express.Router();
 
 module.exports = function (db) {
     router.get('/', isLoggedIn, async function (req, res) {
-        res.render('purchases/list', { user: req.session.user })
+        try {
+            const purchases = await Purchase.findAll({
+                include: [
+                    {
+                        model: Supplier
+                    }
+                ]
+            })
+            res.render('purchases/list', { user: req.session.user, purchases, moment })
+        } catch (error) {
+            console.log(error)
+        }
     })
 
     router.get('/add', isLoggedIn, async function (req, res) {
         try {
             const supplier = await Supplier.findOne({})
-            console.log(supplier)
+            // console.log(supplier)
             const purchase = await Purchase.create({ totalsum: 0, supplier: supplier.supplierid, operator: req.session.user.id })
             res.redirect(`/purchases/edit/${purchase.invoice}`)
         } catch (error) {
@@ -21,11 +33,52 @@ module.exports = function (db) {
 
     router.get('/edit/:invoice', isLoggedIn, async function (req, res) {
         try {
-            const purchase = await Purchase.findByPk(req.params.invoice)
-            console.log(purchase)
-            res.render('purchases/form', { user: req.session.user, purchase })
+            const purchase = await Purchase.findOne({
+                where: { invoice: req.params.invoice },
+                include: [
+                    {
+                        model: User,
+                        attributes: ['id', 'name', 'email']
+                    },
+                    {
+                        model: PurchaseItem,
+                        include: [
+                            {
+                                model: Good,
+                                attributes: ['name']
+                            }
+                        ]
+                    }
+                ]
+            })
+            console.log('Purchase', purchase)
+
+            const suppliers = await Supplier.findAll()
+
+            const goods = await Good.findAll()
+
+            const totalsum = purchase.PurchaseItems?.reduce((sum, item) => sum + Number(item.totalprice), 0) || 0;
+
+            const now = moment();;
+            const currentDateTime = now.format('DD MMM YYYY HH:mm:ss');
+
+            res.render('purchases/form', { user: req.session.user, purchase, currentDateTime, goods, suppliers, totalsum })
         } catch (error) {
             console.log(error)
+        }
+    })
+
+    router.post('/edit/:invoice/item', isLoggedIn, async function (req, res) {
+        const { invoice } = req.params
+        const { itemcode, quantity, purchaseprice, totalprice } = req.body
+
+        try {
+            const item = await PurchaseItem.create({ invoice, itemcode, quantity, purchaseprice, totalprice })
+            res.status(201).json(item)
+        } catch (error) {
+            console.log(err)
+            res.status(400).json({ error: err.message });
+
         }
     })
 
